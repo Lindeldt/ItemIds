34efa98f0b370251c59a034516b2ea2e0184da54
Lindeldt <85031428+Lindeldt@users.noreply.github.com>
2025-06-29 22:07:26 +0100

    Delete killswitch directory

--- killswitch/KillTrigger.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.killswitch;
-
-public abstract class KillTrigger {
-    protected boolean shouldCheck;
-
-    public KillTrigger(boolean shouldCheck) {
-        this.shouldCheck = shouldCheck;
-    }
-}
